89db1aae046b2a756ed06740743a14549e5c08f4
云弧 <ARCloud217@outlook.com>
2026-08-21 23:36:44 +0800

    优化锚点和信号源的信息显示

--- src/silicon/world/meta/Stat.java
+++ src/silicon/world/meta/Stat.java
@@ -4,7 +4,7 @@ import mindustry.world.meta.StatCat;
 
 public class Stat extends mindustry.world.meta.Stat {
     public static final Stat
-            itemsScaled = new Stat("items-scaled", StatCat.items),
+            itemsScaled = new Stat("items-scaled", StatCat.items)
 
 
     public Stat(String name) {
